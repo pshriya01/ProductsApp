6fe656ceef208934ff0b9da3228771b1d982d16f
pshriya01 <shriya.shriya16@gmail.com>
2023-11-02 05:48:48 +0530

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductCatalogComponent } from './product-catalog/product-catalog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductCatalogComponent
   ],
   imports: [
     BrowserModule,
